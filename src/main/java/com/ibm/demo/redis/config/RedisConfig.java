eb22684016c439cf8867a3cbaf5d785e5351fd10
yoonjk <nexweb1@gmail.com>
2023-04-10 23:56:19 +0900

    Modify redis-config

--- src/main/java/com/ibm/demo/redis/config/RedisConfig.java
+++ src/main/java/com/ibm/demo/redis/config/RedisConfig.java
@@ -1,7 +1,17 @@
 package com.ibm.demo.redis.config;
 
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.CacheKeyPrefix;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
@@ -9,29 +19,70 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import io.lettuce.core.ReadFrom;
+import io.lettuce.core.cluster.ClusterClientOptions;
+import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
 
 @Configuration
 public class RedisConfig {
+	@Value("${spring.redis.cluster.nodes}")
+	private List<String> clusterNodes;
     // lettuce 사용시
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
         LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder()
+        		.clientOptions(ClusterClientOptions.builder()
+        				.topologyRefreshOptions(ClusterTopologyRefreshOptions.builder()
+        						.refreshPeriod(Duration.ofMinutes(15))
+        						.enableAdaptiveRefreshTrigger()
+        						.build())
+        				.build())
                 .readFrom(ReadFrom.REPLICA_PREFERRED) // 복제본 노드에서 읽지 만 사용할 수없는 경우 마스터에서 읽습니다.
                 .build();
+        
+
         // 모든 클러스터(master, slave) 정보를 적는다. (해당 서버중 접속되는 서버에서 cluster nodes 명령어를 통해 모든 클러스터 정보를 읽어오기에 다운 됐을 경우를 대비하여 모든 노드 정보를 적어두는편이 좋다.)
-        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration()
-                .clusterNode("localhost", 6300)
-                .clusterNode("localhost", 6301)
-                .clusterNode("localhost", 6302)
-                .clusterNode("localhost", 6400)
-                .clusterNode("localhost", 6401)
-                .clusterNode("localhost", 6402);
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(clusterNodes);
+//                .clusterNode("localhost", 6300)
+//                .clusterNode("localhost", 6301)
+//                .clusterNode("localhost", 6302)
+//                .clusterNode("localhost", 6400)
+//                .clusterNode("localhost", 6401)
+//                .clusterNode("localhost", 6402);
+        
         LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisClusterConfiguration, clientConfiguration);
         return lettuceConnectionFactory;
     }
+    
+    @Bean
+    public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory){
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues() 
+                .entryTtl(Duration.ofSeconds(CacheKey.DEFAULT_EXPIRE_SEC)) 
+                .computePrefixWith(CacheKeyPrefix.simple()) 
+                .serializeKeysWith(
+                        RedisSerializationContext.SerializationPair
+                                .fromSerializer(new StringRedisSerializer())) 
+
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+
+       // 캐시키 별 default 유효시간 설정
+       Map<String, RedisCacheConfiguration> cacheConfiguration = new HashMap<>();
+       cacheConfiguration.put(CacheKey.ZONE,RedisCacheConfiguration.defaultCacheConfig()
+               .entryTtl(Duration.ofSeconds(CacheKey.ZONE_EXPIRE_SEC)));
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(configuration)
+                .withInitialCacheConfigurations(cacheConfiguration)
+                .build();
+    }    
   
     @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory){
