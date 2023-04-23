ef4a7a12bc10fc2b111e2aa6fbb1e6786631f9a0
yoonjk <nexweb1@gmail.com>
2023-04-24 05:10:54 +0900

    Modify leaderboard

--- src/main/resources/scripts/leaderBoard.lua
+++ src/main/resources/scripts/leaderBoard.lua
@@ -3,7 +3,7 @@ local min = math.max(rank - ARGV[1], 0)
 local max = rank + ARGV[1]
 local ldb = redis.call('zrange', KEYS[1], min, max)
 local results = {}
-redis.log(redis.LOG_DEBUG, ldb)
+
 results[1] = tostring(rank + 1)
 
 return results
