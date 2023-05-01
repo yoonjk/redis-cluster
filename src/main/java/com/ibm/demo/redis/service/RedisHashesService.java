e65eda8c72b2a29745dec179174ddc66f948397d
yoonjk <nexweb1@gmail.com>
2023-05-02 04:02:44 +0900

    Add list

--- src/main/java/com/ibm/demo/redis/service/RedisHashesService.java
+++ src/main/java/com/ibm/demo/redis/service/RedisHashesService.java
@@ -16,6 +16,37 @@ public class RedisHashesService {
     
     public Object setItems(HashReqVO hashReqVO) {
     	redisTemplate.opsForHash().put(hashReqVO.getKey(), hashReqVO.getItems(), hashReqVO.getValues());
+    	
+    	return null;
+    }
+    
+    public Object getEntries(HashReqVO hashReqVO) {
+    	Object ret = redisTemplate.opsForHash().entries(hashReqVO.getKey());
+    	
+    	return ret;
+    }
+    
+    public Object delete(HashReqVO hashReqVO) {
+    	Long ret = redisTemplate.opsForHash().delete(hashReqVO.getKey(), hashReqVO.getItems()[0]);
+    	
+    	return ret;
+    }
+    
+    public Object values(HashReqVO hashReqVO) {
+    	Object ret = redisTemplate.opsForHash().values(hashReqVO.getKey());
+    	
+    	return ret;
+    }
+    
+    public Object keys(HashReqVO hashReqVO) {
+    	Object ret = redisTemplate.opsForHash().keys(hashReqVO.getKey());
+    	
+    	return ret;
+    }    
+    
+    public Object setPut(HashReqVO hashReqVO) {
+    	redisTemplate.opsForHash().put(hashReqVO.getKey(), hashReqVO.getItems()[0], hashReqVO.getValues()[0]);
+
     	return null;
     }
 }
